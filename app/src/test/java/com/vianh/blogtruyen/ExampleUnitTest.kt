934bfc48ef0f8393b29979f37e37a6466bbd9e61
Vi Anh <anh.viet@tekmedi.com>
2021-11-14 21:55:02 +0700

    Setup github action workflows

--- app/src/test/java/com/vianh/blogtruyen/ExampleUnitTest.kt
+++ app/src/test/java/com/vianh/blogtruyen/ExampleUnitTest.kt
@@ -37,21 +37,21 @@ class ExampleUnitTest {
         return 29
     }
 
-    @Test
-    fun testCoroutine() {
-        runBlocking {
-            val time = measureTimeMillis {
-                val one = async { doSomethingUsefulOne() }
-                val two = async { doSomethingUsefulTwo() }
-                // some computation
-//                one.start() // start the first one
-//                two.start() // start the second one
-                print("one ${one.isCompleted}")
-                println("The answer is ${one.await() + two.await()}")
-            }
-            println("Completed in $time ms")
-        }
-    }
+//    @Test
+//    fun testCoroutine() {
+//        runBlocking {
+//            val time = measureTimeMillis {
+//                val one = async { doSomethingUsefulOne() }
+//                val two = async { doSomethingUsefulTwo() }
+//                // some computation
+////                one.start() // start the first one
+////                two.start() // start the second one
+//                print("one ${one.isCompleted}")
+//                println("The answer is ${one.await() + two.await()}")
+//            }
+//            println("Completed in $time ms")
+//        }
+//    }
 
     // The result type of somethingUsefulOneAsync is Deferred<Int>
     fun somethingUsefulOneAsync() = scope.async {
@@ -87,20 +87,20 @@ class ExampleUnitTest {
         }
     }
 
-    @Test
-    fun main() {
-        val time = measureTimeMillis {
-            // we can initiate async actions outside of a coroutine
-            val one = somethingUsefulOneAsync()
-            val two = somethingUsefulTwoAsync()
-            // but waiting for a result must involve either suspending or blocking.
-            // here we use `runBlocking { ... }` to block the main thread while waiting for the result
-            runBlocking {
-                println("The answer is ${one.await() + two.await()}")
-            }
-        }
-        println("Completed in $time ms")
-    }
+//    @Test
+//    fun main() {
+//        val time = measureTimeMillis {
+//            // we can initiate async actions outside of a coroutine
+//            val one = somethingUsefulOneAsync()
+//            val two = somethingUsefulTwoAsync()
+//            // but waiting for a result must involve either suspending or blocking.
+//            // here we use `runBlocking { ... }` to block the main thread while waiting for the result
+//            runBlocking {
+//                println("The answer is ${one.await() + two.await()}")
+//            }
+//        }
+//        println("Completed in $time ms")
+//    }
 
     @ObsoleteCoroutinesApi
     @Test
